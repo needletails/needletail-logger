7d5bfc3ae444ff23e0aff21c8a21dc233f854d5d
Cole Moore <Cartisim@icloud.com>
2024-11-19 05:40:07 +0800

    Actor Based, file logging pagination

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:5.10
+// swift-tools-version:6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "needletail-logger",
+    platforms: [.iOS(.v13), .macOS(.v12), .tvOS(.v15), .watchOS(.v8)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -12,7 +13,7 @@ let package = Package(
             targets: ["NeedleTailLogger"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.5.4")
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.6.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
--- Sources/NeedleTailLogger/NeedleTailLogger.swift
+++ Sources/NeedleTailLogger/NeedleTailLogger.swift
@@ -8,32 +8,60 @@
 import Foundation
 import Logging
 
-public struct NeedleTailLogger: Sendable {
+public actor NeedleTailLogger {
     
-    private let logger: Logger
-    private let redColor = "\u{001B}[0;31m"  // Red color
-    private let resetColor = "\u{001B}[0m" // Reset color to default
+    private var logger: Logger
+    private var logFileURL: URL
+    private let maxLines: Int
     
-    public init(_ logger: Logger = Logger(label: "[NeedleTailLogging]"), level: Logger.Level = .debug) {
+    public init(_
+                logger: Logger = Logger(label: "[NeedleTailLogging]"),
+                level: Logger.Level = .debug,
+                maxLines: Int = 1000) {
         var logger = logger
         logger.logLevel = level
         self.logger = logger
+        self.maxLines = maxLines
+        
+        // Set the log file URL
+        guard let url = FileManager.default.urls(for: .libraryDirectory, in: .userDomainMask).first?.appendingPathComponent("logs.txt") else {
+            fatalError("Unable to access log file directory.")
+        }
+        self.logFileURL = url
+        
+        // Create a new log file if it already exists
+        if FileManager.default.fileExists(atPath: logFileURL.path) {
+            Task { [weak self] in
+                guard let self else { return }
+                await self.createNewLogFile()
+            }
+        } else {
+            // Create the log file if it doesn't exist
+            FileManager.default.createFile(atPath: logFileURL.path, contents: nil, attributes: nil)
+        }
+    }
+    
+    public func setLogLevel(_ level: Logger.Level) {
+        logger.logLevel = level
+        logger.info("Log level set to \(level)")
     }
     
     public func log(
         level: Logger.Level,
         message: Logger.Message,
         metadata: Logger.Metadata? = nil
-    ) {
+    ) async {
+        guard level >= logger.logLevel else { return } // Only log if the level is greater than or equal to the current log level
+        
         switch level {
         case .trace:
             logger.trace(message, metadata: metadata)
         case .debug:
-            #if DEBUG
+#if DEBUG
             logger.debug("\n--------------------\n \(message)\n--------------------", metadata: metadata)
-            #else
+#else
             break
-            #endif
+#endif
         case .info:
             logger.info(message, metadata: metadata)
         case .notice:
@@ -46,28 +74,58 @@ public struct NeedleTailLogger: Sendable {
             logger.critical(message, metadata: metadata)
         }
         
-        
-        guard var url = FileManager.default.urls(for: .libraryDirectory, in: .userDomainMask).first else {
-            return
-        }
-        
-        url.appendPathComponent("logs.txt")
-        
-        if !FileManager.default.fileExists(atPath: url.path) {
-            FileManager.default.createFile(
-                atPath: url.path,
-                contents: nil,
-                attributes: nil
-            )
-        }
-        
+        await logMessage(message.description)
+    }
+    
+    private func logMessage(_ message: String) async {
         do {
-            let handle = try FileHandle(forWritingTo: url)
-            defer { handle.closeFile() }
-            handle.seekToEndOfFile()
-            handle.write(message.description.data(using: .utf8)!)
+            // Read the current contents of the log file to check the line count
+            let fileContents = try String(contentsOf: logFileURL, encoding: .utf8)
+            let lineCount = fileContents.components(separatedBy: .newlines).count
+            
+            // If current file exceeds maxLines, create a new file
+            if lineCount >= maxLines {
+                createNewLogFile()
+            }
+            
+            // Write the log message to the current log file
+            let fileHandle = try FileHandle(forWritingTo: logFileURL)
+            defer { fileHandle.closeFile() }
+            
+            // Move to the end of the file and write the message
+            fileHandle.seekToEndOfFile()
+            if let data = (message + "\n").data(using: .utf8) {
+                fileHandle.write(data)
+            }
         } catch {
-            NeedleTailLogger(.init(label: "[ File-Logger ]")).log(level: .error, message: "\(error)")
+            logger.error("Failed to write to log file: \(error.localizedDescription)")
         }
+    }
+    
+    private func createNewLogFile() {
+        let timestamp = ISO8601DateFormatter().string(from: Date())
+        let newLogFileName = "logs_\(timestamp).txt".replacingOccurrences(of: ":", with: "-") // Replace colons for filename safety
+        let newLogFileURL = logFileURL.deletingLastPathComponent().appendingPathComponent(newLogFileName)
+        
+        // Create the new log file
+        if !FileManager.default.fileExists(atPath: newLogFileURL.path) {
+            FileManager.default.createFile(atPath: newLogFileURL.path, contents: nil, attributes: nil)
+            logger.info("Created new log file: \(newLogFileName)")
+        }
+        
+        // Optionally, write a message to the new log file indicating that a new log file has been created
+        do {
+            let handle = try FileHandle(forWritingTo: newLogFileURL)
+            defer { handle.closeFile() }
+            let creationMessage = "New log file created on \(Date())\n"
+            if let data = creationMessage.data(using: .utf8) {
+                handle.write(data)
+            }
+        } catch {
+            logger.error("Failed to write to new log file: \(error.localizedDescription)")
+        }
+        
+        // Update the logFileURL to point to the new log file
+        self.logFileURL = newLogFileURL
     }
 }
